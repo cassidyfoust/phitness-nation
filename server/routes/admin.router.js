455a0226530d2e0aec0a3d8eb42ee12a9a4a6f8a
Angela Mattson <angelamattson@Davids-MacBook-Pro-3.local>
2019-11-18 16:01:22 -0600

    merge conflict

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -342,6 +342,20 @@ router.get('/exercises/:id', (req, res) =>{
         })
 })
 
+
+
+router.put('/edituser', (req, res) => {
+    let queryText = `UPDATE "user" SET "name" = $1, "pronouns" = $2, "phone" = $3, "email" = $4, "emergency_contact_name" = $5, "emergency_contact_phone" = $6, "age" = $7 WHERE "id" = $8;`
+    let queryInfo = [req.body.name, req.body.pronouns, req.body.phone, req.body.email, req.body.emergencyContactName, req.body.emergencyContactPhone, req.body.dateOfBirth, req.body.id];
+    pool.query(queryText, queryInfo)
+        .then(() => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            res.sendStatus(500);
+            console.log('PUT USER INFO ERROR:', error);
+        })
+})
+
 //req.params is an array with one just index (the user's id)
 router.put('/reactivate/:id', (req, res) => {
     console.log("req: ", req.body, req.params.id);
@@ -353,18 +367,6 @@ router.put('/reactivate/:id', (req, res) => {
         }).catch((error) => {
             res.sendStatus(500);
             console.log('REACTIVATE USER INFO ERROR:', error);
-        })
-})
-
-router.put('/edituser', (req, res) => {
-    let queryText = `UPDATE "user" SET "name" = $1, "pronouns" = $2, "phone" = $3, "email" = $4, "emergency_contact_name" = $5, "emergency_contact_phone" = $6, "age" = $7 WHERE "id" = $8;`
-    let queryInfo = [req.body.name, req.body.pronouns, req.body.phone, req.body.email, req.body.emergencyContactName, req.body.emergencyContactPhone, req.body.dateOfBirth, req.body.id];
-    pool.query(queryText, queryInfo)
-        .then(() => {
-            res.sendStatus(200);
-        }).catch((error) => {
-            res.sendStatus(500);
-            console.log('PUT USER INFO ERROR:', error);
         })
 })
 
